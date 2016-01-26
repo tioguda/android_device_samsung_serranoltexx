c2e880d3b694015a185d2387bfea665ebe89ee90
tioguda <tioguda@users.noreply.github.com>
2016-01-26 19:57:02 -0200

    Update and rename cm.mk to bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -1,11 +1,11 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
 
-PRODUCT_NAME := cm_serranoltexx
+PRODUCT_NAME := bliss_serranoltexx
